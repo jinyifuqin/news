db2a94e9fccb52f54d7dbd62f00298a8a9a9ca5c
yangyh <jinyifuqin@163.com>
2018-03-29 18:08:26 +0800

    bootstrap

--- resources/views/admin/piccontent.blade.php
+++ resources/views/admin/piccontent.blade.php
@@ -1,28 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>用户首页</title>
-</head>
-<body>
+@extends('layout')
+@section('content')
+    <div class="container">
+        @foreach($res as $value)
+        <div class="row">
+            <div class="col-md-12">
+                <h3 style="text-align: center">{{$value->title}}</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <img style="width: 100%;" class="img-circle" src="/uploads/{{$value->pathname}}"/>
+            </div>
+            <div class="col-md-9 well well-lg" style="background-color: #dedef8;box-shadow:inset 1px -1px 1px #444, inset -1px 1px 1px #444;min-height: 200px;">
+            {{$value->content}}
+            </div>
+        </div>
+        @endforeach
+            <div class="row">
+                <div class="center-block" style="width:30%;">
+                    <ul class="pagination">
+                        <li><a href="#">&laquo;</a></li>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li><a href="#">&raquo;</a></li>
+                    </ul>
+                </div>
+            </div>
+    </div>
 
-
-@foreach($res as $value)
-    <h3 style="text-align: center">{{$value->title}}</h3>
-<table>
-    <tr>
-
-
-    </tr>
-
-    <tr>
-        <td><img src="/uploads/{{$value->pathname}}"/></td>
-    </tr>
-    <tr>
-        <td>{{$value->content}}</td>
-    </tr>
-
-</table>
-@endforeach
-</body>
-</html>
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Yyh</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- 引入 Bootstrap -->
+    <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <script src="{{ URL::asset('js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+</head>
+<body>
+@yield('content')
+<script>
+    var c = $('h3');
+
+
+    console.log(c);
+</script>
+</body>
+</html>
